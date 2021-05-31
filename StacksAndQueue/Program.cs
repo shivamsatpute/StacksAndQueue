0631b150393964e1a0ca36307c7e0f7ea5ef3dde
shivam satpute <shivam897satpute@gmail.com>
2021-05-31 19:18:48 +0530

    [shivam]add: Create a Queue and print Top Element in Queue

--- StacksAndQueue/Program.cs
+++ StacksAndQueue/Program.cs
@@ -6,15 +6,22 @@ namespace StacksAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Stack");
-            Stack stack = new Stack(); 
-            stack.Push(56); 
-            stack.Push(30);  
-            stack.Push(70);
-            stack.Display();
-            stack.Peek(); 
-            stack.Pop(); 
-            stack.isEmpty(); 
+           // Console.WriteLine("Stack");
+            //Stack stack = new Stack(); 
+            //stack.Push(56); 
+            //stack.Push(30);  
+            //stack.Push(70);
+            //stack.Display();
+            //stack.Peek(); 
+            //stack.Pop(); 
+            //stack.isEmpty();
+            Console.WriteLine("Queue"); 
+            Queue queue = new Queue();  
+            queue.Enqueue(56); 
+            queue.Enqueue(30); 
+            queue.Enqueue(70);
+            queue.Display(); 
+            queue.Peek(); 
 
             Console.ReadLine();
         }
---
+++ StacksAndQueue/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueue
+{
+    class Queue
+    {
+        public Node head; 
+        public void Enqueue(int data)
+        {
+
+            Node node = new Node(data);  
+            Node temp = head;
+
+            if (head == null) 
+            {
+                head = node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode(temp); 
+                lastNode.next = node;
+            }
+            Console.WriteLine($"Element Added in Queue:- {node.data}");
+        }
+
+        public Node GetLastNode(Node temp)
+        {
+            if (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+
+                while (temp != null)  
+                {
+                    Console.WriteLine($"Queue Element is:- {temp.data}"); 
+                    temp = temp.next;
+                }
+            }
+        }
+        public int Peek()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Console.WriteLine($"{head.data } is a Top of the Queue "); 
+
+            return head.data;
+        }
+    }
+}
+
